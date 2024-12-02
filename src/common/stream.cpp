93aff6e49f4927feff3066601517682513d19846
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-12-02 23:19:31 +0900

    src/common/shuffle.cpp src/common/softmax.cpp src/common/stream.cpp src/common/sum.cpp src/common/verbose.cpp src/cpu/cpu_engine.cpp

--- src/common/stream.cpp
+++ src/common/stream.cpp
@@ -14,7 +14,7 @@
 * limitations under the License.
 *******************************************************************************/
 
-#include <assert.h>
+#include <cassert>
 #include <memory>
 
 #include "oneapi/dnnl/dnnl.h"
