5b4be6569364d7fde7616d97b0e9fefb423340b3
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-01-26 11:40:54 +0900

    src/cpu/x64/jit_avx512_core_amx_conv_kernel.hpp src/cpu/x64/jit_brgemm_1x1_conv.hpp src/cpu/x64/jit_brgemm_transpose_utils.hpp src/cpu/x64/jit_transpose_utils.hpp src/cpu/x64/matmul/brgemm_matmul_copy_utils.hpp src/cpu/x64/rnn/jit_uni_gru_cell_postgemm_1_bwd.hpp src/cpu/x64/rnn/jit_uni_gru_cell_postgemm_2_bwd.hpp src/cpu/x64/rnn/jit_uni_gru_lbr_cell_postgemm_bwd.hpp src/cpu/x64/rnn/jit_uni_lstm_cell_projection_postgemm_fwd.hpp src/cpu/x64/rnn/jit_uni_rnn_cell_postgemm_bwd.hpp

--- src/cpu/x64/rnn/jit_uni_lstm_cell_projection_postgemm_fwd.hpp
+++ src/cpu/x64/rnn/jit_uni_lstm_cell_projection_postgemm_fwd.hpp
@@ -33,7 +33,7 @@ struct jit_uni_lstm_cell_projection_postgemm_fwd : public jit_uni_rnn_postgemm {
             const rnn_utils::rnn_conf_t &rnn, const rnn_pd_t *pd)
         : jit_uni_rnn_postgemm(rnn, pd, jit_name()) {}
 
-    ~jit_uni_lstm_cell_projection_postgemm_fwd() override {}
+    ~jit_uni_lstm_cell_projection_postgemm_fwd() override = default;
 
     status_t init(data_type_t sdt) override {
         jit_uni_rnn_postgemm::init(src_data_t);
