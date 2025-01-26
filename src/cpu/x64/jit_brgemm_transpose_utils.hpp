7adc1c023c564c6c76d6edb7f65bf9b682ce8814
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-01-26 10:49:15 +0900

    src/cpu/rnn/rnn_reorders.hpp src/cpu/x64/jit_brgemm_transpose_utils.hpp

--- src/cpu/x64/jit_brgemm_transpose_utils.hpp
+++ src/cpu/x64/jit_brgemm_transpose_utils.hpp
@@ -142,10 +142,10 @@ struct jit_brgemm_trans_to_vnni_t {
         dim_t current_col_size, current_row_size;
     };
 
-    typedef enum matrix_to_transform {
+    using matrix_to_transform_t = enum matrix_to_transform {
         matrix_B,
         matrix_C
-    } matrix_to_transform_t;
+    };
 
     virtual void operator()(ctx_t *ctx) = 0;
     virtual status_t create_kernel() = 0;
